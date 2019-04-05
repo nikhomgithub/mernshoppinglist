3e682976944c3fff9fe8e08064438d4817262709
Nikhom Phutthawithee <mr_nikom@yahoo.com>
2019-04-05 16:12:02 +0700

    mongodb atlas +heroku

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-  mongoURI:'mongodb://localhost/mern_shoppping'
+  //mongoURI:'mongodb://localhost/mern_shoppping'
+  mongoURI:'mongodb+srv://nikhom123:meemee@cluster0-7ll9a.gcp.mongodb.net/test?retryWrites=true'
 }
